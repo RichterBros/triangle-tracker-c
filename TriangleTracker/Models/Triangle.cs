f19e8a0f349069627a0287d0faeb681f87a74d06
Kate Skorija <kateskorija@gmail.com>
2020-07-14 11:06:12 -0700

    create passing test for Isosceles triangle
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: chyoon2 <chy.yoon@gmail.com>

--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -19,9 +19,13 @@ namespace TriangleTracker
       {
         return "Equilateral";
       }
+      else if (side1 == side2 || side2 == side3 || side1 == side3)
+      {
+        return "Isosceles";
+      }
       else
       {
-        return "Hey";
+        return "hey";
       }
     }
   }
