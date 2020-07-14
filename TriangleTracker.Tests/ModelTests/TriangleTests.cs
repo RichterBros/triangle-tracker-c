4e4d71c97052c3e003a02ad6ee4c15d044ca7b4f
Kate Skorija <kateskorija@gmail.com>
2020-07-14 11:42:40 -0700

    refactor to use triangle constructor, create Program class for console app
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: chyoon2 <chy.yoon@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -9,26 +9,26 @@ namespace TriangleTracker.Tests
     [TestMethod]
     public void IsTriangle_CreatesTriangle_NotTriangle()
     {
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual("Not a triangle", testTriangle.IsTriangle(3,4,20));
+      Triangle testTriangle = new Triangle(3, 4, 20);
+      Assert.AreEqual("Not a triangle", testTriangle.IsTriangle());
     }
     [TestMethod]
     public void IsTriangle_IsEquilateral_Equilateral()
     {
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual("Equilateral", testTriangle.IsTriangle(3,3,3));
+      Triangle testTriangle = new Triangle(3,3,3);
+      Assert.AreEqual("Equilateral", testTriangle.IsTriangle());
     }
     [TestMethod]
     public void IsTriangle_IsIsosceles_Isosceles()
     {
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual("Isosceles", testTriangle.IsTriangle(3,4,4));
+      Triangle testTriangle = new Triangle(3,4,4);
+      Assert.AreEqual("Isosceles", testTriangle.IsTriangle());
     }
     [TestMethod]
     public void IsTriangle_IsScalene_Scalene()
     {
-      Triangle testTriangle = new Triangle();
-      Assert.AreEqual("Scalene", testTriangle.IsTriangle(3,4,5));
+      Triangle testTriangle = new Triangle(3,4,5);
+      Assert.AreEqual("Scalene", testTriangle.IsTriangle());
     }
   }
 }
--- TriangleTracker/Models/Triangle.cs
+++ TriangleTracker/Models/Triangle.cs
@@ -8,18 +8,25 @@ namespace TriangleTracker
     public int Side1;
     public int Side2;
     public int Side3;
-  
-    public string IsTriangle(int side1, int side2, int side3)
+
+    public Triangle(int side1, int side2, int side3)
     {
-      if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2)
+      Side1 = side1;
+      Side2 = side2;
+      Side3 = side3;
+    }
+  
+    public string IsTriangle()
+    {
+      if (Side1 + Side2 <= Side3 || Side2 + Side3 <= Side1 || Side3 + Side1 <= Side2)
       {
         return "Not a triangle";
       }
-      else if ((side1 == side2) && (side2 == side3))
+      else if ((Side1 == Side2) && (Side2 == Side3))
       {
         return "Equilateral";
       }
-      else if (side1 == side2 || side2 == side3 || side1 == side3)
+      else if (Side1 == Side2 || Side2 == Side3 || Side1 == Side3)
       {
         return "Isosceles";
       }
@@ -27,6 +34,34 @@ namespace TriangleTracker
       {
         return "Scalene";
       }
+    }
+  }
+  public class Program
+  {
+    public static void Main()
+    {
+      //for(i=1; i<4; i++)
+      //Console.WriteLine("Enter a number from 0-100");
+      // string stringSideOne = Console.ReadLine();
+      //int "side"+ i = int.Parse(stringSideOne);
+
+      Console.WriteLine("Enter a number from 0-100");
+      string stringSideOne = Console.ReadLine();
+      int sideOne = int.Parse(stringSideOne);
+
+      Console.WriteLine("Enter a number from 0-100");
+      string stringSideTwo = Console.ReadLine();
+      int sideTwo = int.Parse(stringSideTwo);
+
+      Console.WriteLine("Enter a number from 0-100");
+      string stringSideThree = Console.ReadLine();
+      int sideThree = int.Parse(stringSideThree);
+
+      Triangle triangle = new Triangle(sideOne, sideTwo, sideThree);
+
+      string triangleType = triangle.IsTriangle();
+
+      Console.WriteLine(triangleType);
     }
   }
 }
