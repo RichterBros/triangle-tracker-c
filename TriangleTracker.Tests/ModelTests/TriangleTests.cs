8336ce92ef7506ee77fbecbbd923a1a52a5a29a4
Kate Skorija <kateskorija@gmail.com>
2020-07-14 10:41:56 -0700

    create failing test for checking if three lengths make a triangle
    Co-authored-by: RichterBros <erich.richter@gmail.com>
    Co-authored-by: chyoon2 <chy.yoon@gmail.com>

--- TriangleTracker.Tests/ModelTests/TriangleTests.cs
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -1,12 +1,17 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TriangleTracker;
 
-namespace TriangleTracker.Test
+namespace TriangleTracker.Tests
 {
   [TestClass]
   public class TriangleTests
   {
     [TestMethod]
-    
+
+    public void IsTriangle_CreatesTriangle_True()
+    {
+      Triangle testTriangle = new Triangle();
+      Assert.AreEqual(true, testTriangle.IsTriangle(3,3,3));
+    }
   }
 }
