c17f0b0d02713d90dccb93ef4ccbc30a21bb2488
Kate Skorija <kateskorija@gmail.com>
2020-07-14 10:00:51 -0700

    create initial files and file tree
    Co-authored-by: Erich Richter <erich.richter@gmail.com>
    Co-Authored-By: Chris Yoon <chy.yoon@gmail.com>

---
+++ TriangleTracker.Tests/ModelTests/TriangleTests.cs
@@ -0,0 +1,12 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TriangleTracker;
+
+namespace TriangleTracker.Test
+{
+  [TestClass]
+  public class TriangleTests
+  {
+    [TestMethod]
+    
+  }
+}
